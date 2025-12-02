f173cdd9cdbce16ad72690b14f5b0371d18e9f17
AlmaRahmatiaNingrum <almarahmatia123@gmail.com>
2025-12-02 16:46:00 +0700

    Add test

---
+++ test/Calculatortest.php
@@ -0,0 +1,13 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once __DIR__ . '/../src/Calculator.php';
+
+class CalculatorTest extends TestCase {
+    public function testAdd() {
+        $calc = new Calculator();
+        $result = $calc->add(2, 3);
+
+        $this->assertEquals(5, $result);
+    }
+}
